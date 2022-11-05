8712b8b4b651ec7db5ed6e801a3ebb9d4533f5bd
el-tumero <tumerpl@gmail.com>
2022-11-05 15:43:41 +0100

    webrtc done

--- src/components/MessageBox/MessageBox.tsx
+++ src/components/MessageBox/MessageBox.tsx
@@ -1,5 +1,6 @@
 import { ethers } from "ethers"
 import { DataConnection, Peer } from "peerjs"
+import { disconnect } from "process"
 import { useEffect, useState } from "react"
 import Cookies from "universal-cookie"
 import KeyPairDatabase from "../../KeyPairDatabase"
@@ -18,8 +19,12 @@ function MessageBox({senderAddress, receiverAddress, contract, setReceiver}:Prop
     const [messages, setMessages] = useState<string[]>([])
     const [typedMessage, setTypedMessage] = useState<string>("")
     const [connection, setConnection] = useState<DataConnection>()
+    const [peer, setPeer] = useState<Peer>()
     const [receiverPublicKey, setReceiverPublicKey] = useState<CryptoKey>()
     const [senderKeyPair, setSenderKeyPair] = useState<CryptoKeyPair>()
+    const [isReceiverWorking, setIsReceiverWorking] = useState<boolean>(false)
+    const [isSenderWorking, setIsSenderWorking] = useState<boolean>(true)
+    const [isInConversation, setIsInConversation] = useState<boolean>(true)
     
     useEffect(() => {
         if(!connection && senderKeyPair && receiverPublicKey){
@@ -35,14 +40,14 @@ function MessageBox({senderAddress, receiverAddress, contract, setReceiver}:Prop
         getKeyPairFromDatabase()
     }, [])
 
-    // useEffect(() => {
-    //     if(receiverPublicKey) console.log(receiverPublicKey)
-    // }, [receiverPublicKey])
-
-    // useEffect(() => {
-    //     if(senderKeyPair) console.log(senderKeyPair)
-    // }, [senderKeyPair])
-
+    useEffect(() => {
+        if(isReceiverWorking && isSenderWorking){
+            setIsInConversation(true)
+            console.log(123)
+        } else {
+            setIsInConversation(false)
+        }
+    }, [isReceiverWorking, isSenderWorking])
 
     async function getPublicKeyFromBlockchain(){
         if(contract){
@@ -75,18 +80,19 @@ function MessageBox({senderAddress, receiverAddress, contract, setReceiver}:Prop
     function createConnection(signature: string){
         if(senderAddress && receiverAddress){
             const peer = new Peer(senderAddress, {host: "localhost", port: 9000, path: "/", token: signature})
+            setPeer(peer)
             const conn = peer.connect(receiverAddress)
+            setConnection(conn)
 
-           
             conn.on("open", () => {
                 console.log("Connection (send channel) is open!")
-                setConnection(conn)
+                setIsSenderWorking(true)
             })
 
             peer.on("connection", (conn) => {
                 console.log("Connection (receive channel) is open!")
+                setIsReceiverWorking(true)
                 conn.on("data", async(data) => {
-
                     try {
                         const decrypted = await window.crypto.subtle.decrypt(
                             {
@@ -98,6 +104,7 @@ function MessageBox({senderAddress, receiverAddress, contract, setReceiver}:Prop
 
                         const decoded = new TextDecoder("utf-8").decode(decrypted)
                         setMessages(current => [...current, decoded])
+                        console.log(decoded)
                         
                     } catch (error) {
                         console.log(error)
@@ -106,9 +113,23 @@ function MessageBox({senderAddress, receiverAddress, contract, setReceiver}:Prop
                 })
             })
 
-
             conn.on("close", () => {
                 console.log("User disconnected")
+                if(receiverAddress){
+                    conn.removeAllListeners("close")
+                    conn.close()
+                    peer.destroy()
+                    setPeer(undefined)
+                    setConnection(undefined)
+                    setIsReceiverWorking(false)
+                    setIsSenderWorking(false)
+                    const cookies = new Cookies()
+                    createConnection(cookies.get("signature"))
+                }
+            })
+
+            conn.on("error", (error) => {
+                console.log(error)
             })
 
         }
@@ -130,18 +151,29 @@ function MessageBox({senderAddress, receiverAddress, contract, setReceiver}:Prop
         connection?.send(encryptedMessage)
     }
 
+    function disconnectPeer(){
+        if(peer && connection){
+            connection.removeAllListeners("close")
+            connection.close()
+            peer.destroy()
+        }
+        setTimeout(() => setReceiver(undefined)) 
+        setReceiver(undefined)
+    }
+
 
 
     return(
         <div>
             <p className="text">Sending messages as {senderAddress}</p>
             <p className="text">To: {receiverAddress}</p>
+            <p className="text">Is in conversation? {String(isInConversation)}</p>
             <div className="messages">
                 {messages.map((message, index) => (<p className="message" key={index}>{message}</p>))}
             </div>
             <input type="text" className="input" onChange={e => setTypedMessage(e.target.value)} />
             <button className="sendButton" onClick={() => { sendMessage() }}>Send</button>
-            <button onClick={() => setReceiver(undefined)}>Close conversation</button>
+            <button onClick={() => disconnectPeer()}>Close conversation</button>
         </div>
     )
 }
